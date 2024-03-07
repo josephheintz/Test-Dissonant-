c931f650132e480bd87be4f91b17644e70cde1b5
Joseph Heintz <heintzj@hawaii.edu>
2024-03-07 02:19:27 -1000

    Merge Basic-Attacks 2

--- Assets/Scripts/PlayerStateList.cs
+++ Assets/Scripts/PlayerStateList.cs
@@ -6,4 +6,7 @@ public class PlayerStateList : MonoBehaviour
 {
     public bool jumping = false;
     public bool dashing = false;
+    public bool recoilingX, recoilingY = false;
+    public bool lookingRight;
+    public bool invincible;
 }
