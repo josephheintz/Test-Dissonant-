4b7012c2ea8c6e5757c4c39723f6752ea8a3f618
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2024-03-14 14:54:20 -1000

    Update Return Teleport
    Adds a better teleprt system

--- Assets/Scripts/ButtonPopUp.cs
+++ Assets/Scripts/ButtonPopUp.cs
@@ -9,6 +9,7 @@ public class ButtonPopUp : MonoBehaviour
     [SerializeField] private Text popUpText; // Change this to Text type
     [SerializeField] private int selectEffect = 0; // Name of the input button
     private string interactionButton = "Interact"; // Name of the input button
+    private bool playerInsideTrigger = false; // Track if player is inside trigger
 
     // Start is called before the first frame update
     private void Start()
@@ -21,20 +22,21 @@ public class ButtonPopUp : MonoBehaviour
 
     void Update()
     {
-        GetInput();
+        if (playerInsideTrigger) // Only get input if player is inside trigger
+        {
+            GetInput();
+        }
     }
 
     void GetInput()
     {
         // Check if the player is pressing the interaction button
-        if (Input.GetButtonDown(interactionButton) == true) // active when "e" is pressed
+        if (Input.GetButtonDown(interactionButton)) // active when "e" is pressed
         {
             if(selectEffect == 0) Debug.Log("Hello, how are you?"); // default, says hi on the log
             if(selectEffect == 1) gameObject.GetComponent<ReturnJump>().enabled = true; // uses the return jump
             // Can add here
         }
-
-
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -42,6 +44,7 @@ public class ButtonPopUp : MonoBehaviour
         // Detect player tag on player
         if (other.CompareTag("Player"))
         {
+            playerInsideTrigger = true; // Player is inside trigger
             if (popUpText != null)
                 popUpText.gameObject.SetActive(true);
             else
@@ -54,10 +57,11 @@ public class ButtonPopUp : MonoBehaviour
         // Detect player leaving trigger area
         if (other.CompareTag("Player"))
         {
-            //Debug.LogError("PopUpText reference is not set in the inspector!");
-            if (popUpText != null){
+            playerInsideTrigger = false; // Player is not inside trigger
+            if (popUpText != null)
                 popUpText.gameObject.SetActive(false);
-                }
+            else
+                Debug.LogError("PopUpText reference is not set in the inspector!");
         }
     }
 }
