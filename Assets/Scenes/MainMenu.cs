73aa251e926bf380e3f3070308c141f886336c23
ZhengChenHe <59942522+Zheng-Chen-He@users.noreply.github.com>
2024-03-14 19:54:18 -1000

    edit the mainmenu.cs

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public void PlayGame(){
+	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+}
+
+public void GoToSettingsMenu(){
+	SceneManager.LoadScene("SettingsMenu");
+}
+
+public void GoToMainMenu(){
+	SceneManager.LoadScene("MainMenu");
+}
+
+public void QuitGame(){
+	Application.Quit();
+}
 }
