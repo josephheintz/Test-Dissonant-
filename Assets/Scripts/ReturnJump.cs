46e36eeabe2d2881c18bd283fac1b5d959abde99
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2024-03-14 16:26:38 -1000

    Teleport Update

--- Assets/Scripts/ReturnJump.cs
+++ Assets/Scripts/ReturnJump.cs
@@ -6,10 +6,11 @@ using UnityEngine.SceneManagement;
 public class ReturnJump : MonoBehaviour
 {
     [SerializeField] private Vector2 landing; // Where the player will appear on the next map
-    [SerializeField] private GameObject playerPrefab; // Prefab of the player object
+    private GameObject playerPrefab; // Prefab of the player object
 
     private void Start()
     {
+        GameObject playerPrefab = GameObject.FindGameObjectWithTag("Player");
         SceneManager.LoadScene(1);
         playerPrefab.transform.position = landing;
 
