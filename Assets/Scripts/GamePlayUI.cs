765c3bb8a606e8bcc3f77df12c0bc90435c3d8fd
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2024-03-17 09:54:43 -1000

    Can stop time

--- Assets/Scripts/GamePlayUI.cs
+++ Assets/Scripts/GamePlayUI.cs
@@ -6,16 +6,34 @@ using UnityEngine.UI; // Import UnityEngine.UI to use Text compone
 public class GamePlayUI : MonoBehaviour
 {
     [SerializeField]  private GameObject player; // Prefab of the player object
+    [SerializeField]  private GameObject menu; // Prefab of the player object
     private Text healthText;
+    private bool isPulsed = false;
+
 
     void Start(){
         healthText = GetComponent<Text>();
         //GameObject player = GameObject.FindGameObjectWithTag("Player");
+        menu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthText.text = "Health: " + player.GetComponent<Health>().currentHealth.ToString();
+        //healthText.text = "Health: " + player.GetComponent<Health>().currentHealth.ToString();
+        // Check if the Esc key is pressed to toggle pause
+
+        if (Input.GetKeyDown(KeyCode.Escape) && isPulsed == false)
+        {
+             Time.timeScale = 0f;
+             player.GetComponent<PlayerController>().enabled = false;
+             menu.SetActive(true);
+             isPulsed = true;
+        } else if (Input.GetKeyDown(KeyCode.Escape) && isPulsed == true){
+            Time.timeScale = 1f;
+            player.GetComponent<PlayerController>().enabled = true;
+            isPulsed = false;
+            menu.SetActive(false);
+        }
     }
 }
