929cd8aaffe9d448230a836b6982faad5c9ea671
jsapolu99 <109643051+jsapolu99@users.noreply.github.com>
2024-04-11 16:28:35 -1000

    Fixed Boss Health/Damage and Added Slime Animatons

--- Assets/Medieval King Pack 2/Scripts/Boss_Health.cs
+++ Assets/Medieval King Pack 2/Scripts/Boss_Health.cs
@@ -4,28 +4,24 @@ using UnityEngine;
 
 public class BossHealth : Enemy
 {
-
-    public bool isInvulnerable = false;
-
-    private Animator animator;
-
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
         rb.gravityScale = 12f;
+        
     }
 
     // Update is called once per frame
     protected override void Update()
     {
         base.Update();
-
     }
 
     public override void EnemyHit(float _damageDone, Vector2 _hitDirection, float _hitForce)
     {
         base.EnemyHit(_damageDone, _hitDirection, _hitForce);
+        
         animator.SetTrigger("TakeDamage");
     }
 
--- Assets/Medieval King Pack 2/Scripts/Boss_Run.cs
+++ Assets/Medieval King Pack 2/Scripts/Boss_Run.cs
@@ -4,7 +4,7 @@ public class Boss_Run : StateMachineBehaviour
 {
     public float speed = 2.5f;
     public float attackRange = 3f;
-    public float attackCooldown = 2f; // Adjust this value to control the attack cooldown
+    public float attackCooldown = 20f; // Adjust this value to control the attack cooldown
 
     Transform player;
     Rigidbody2D rb;
@@ -19,7 +19,7 @@ public class Boss_Run : StateMachineBehaviour
         rb = animator.GetComponent<Rigidbody2D>();
         boss = animator.GetComponent<Boss>();
         hasAttacked = false; // Reset the attack flag when entering the state
-        nextAttackTime = Time.time; // Initialize nextAttackTime to current time
+        // nextAttackTime = Time.time; // Initialize nextAttackTime to current time
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -37,13 +37,20 @@ public class Boss_Run : StateMachineBehaviour
             animator.SetTrigger("Attack");
             hasAttacked = true; // Set the flag to true to prevent continuous attacks
             nextAttackTime = Time.time + attackCooldown; // Set the next attack time
+         
+        }
+        // Update the nextAttackTime if the boss hasn't attacked yet
+        if (hasAttacked)
+        {
+            nextAttackTime = Time.time + attackCooldown; // Set the next attack time
         }
     }
+
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // Reset the attack flag when exiting the state
+        //Reset the attack flag when exiting the state
         hasAttacked = false;
         animator.ResetTrigger("Attack");
     }
