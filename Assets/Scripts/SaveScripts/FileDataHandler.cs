a6972c1c83ad2073fa7068e9984bf7f9b0c0e295
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2024-04-16 11:24:15 -1000

    More Added

--- Assets/Scripts/SaveScripts/FileDataHandler.cs
+++ Assets/Scripts/SaveScripts/FileDataHandler.cs
@@ -13,4 +13,46 @@ public class FileDataHandler
         this.dataDirPath = dataDirPath;
         this.dataFileName = dataFileName;
     }
+
+    public SaveData Load(){
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
+        SaveData loadedData = null;
+        if(File.Exists(fullPath)){
+
+            try{
+                string dataToLoad = "";
+                using(FileStream stream = new FileStream(fullPath, FileMode.Open)){
+                    using(StreamReader reader = new StreamReader(stream)){
+                        dataToLoad = reader.ReadToEnd();
+                    }
+                }
+
+                loadedData = JsonUtility.FromJson<SaveData>(dataToLoad);
+
+            }
+
+            catch(Exception e){
+                Debug.LogError("Save error at " + fullPath + "/n" + e);
+            }
+        }
+
+        return loadedData;
+    }
+
+    public void Save(SaveData data){
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
+        try{
+            Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+            string dataToStore = JsonUtility.ToJson(data, true);
+
+            using(FileStream stream = new FileStream(fullPath, FileMode.Create)){
+                using(StreamWriter writer = new StreamWriter(stream)){
+                    writer.Write(dataToStore);
+                }
+            }
+        }
+        catch (Exception e){
+            Debug.LogError("Save error at " + fullPath + "/n" + e);
+        }
+    }
 }
