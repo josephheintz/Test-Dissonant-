bc2d11c8cabf19dbefb85aec57d142c1b4a836a2
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2024-04-16 14:14:08 -1000

    It Works!!! Sort of anyway.

--- Assets/Scripts/SaveScripts/PersistenceDataManager.cs
+++ Assets/Scripts/SaveScripts/PersistenceDataManager.cs
@@ -7,10 +7,17 @@ using UnityEngine.SceneManagement;
 
 public class PersistenceDataManager : MonoBehaviour
 {
+
+    [Header("File Storage Config")]
+
+    [SerializeField] private string fileName;
+
     public static PersistenceDataManager instance {get; private set;}
+
     private SaveData gameData;
 
     public List<IDataPersistence> dataPersistenceObjects;
+    private FileDataHandler dataHandler;
 
     private void Awake(){
         if(instance != null) Debug.LogError("There are to many Persistence Data Managers.");
@@ -18,6 +25,7 @@ public class PersistenceDataManager : MonoBehaviour
     }
 
     private void Start(){
+        this.dataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
         this.dataPersistenceObjects = FindAllDataPersistenceObjects();
         LoadGame();
     }
@@ -27,13 +35,18 @@ public class PersistenceDataManager : MonoBehaviour
     }
 
     public void SaveGame(){
-        //Debug.Log("Hello 2");
-        foreach(IDataPersistence dataPersistenceObj in dataPersistenceObjects){
-            dataPersistenceObj.SaveData(ref gameData);
-        }
+            //Debug.Log("Hello 2");
+            foreach(IDataPersistence dataPersistenceObj in dataPersistenceObjects){
+                dataPersistenceObj.SaveData(ref gameData);
+            }
+
+
+        dataHandler.Save(gameData);
     }
 
     public void LoadGame(){
+        this.gameData = dataHandler.Load();
+
         if(this.gameData == null){
             Debug.Log("No saved data, using starting new game");
             NewGame();
